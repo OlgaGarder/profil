ce2a91b83b194781a86da14cf1b084f1cda7cdd6
OlgaGarder <50359762+OlgaGarder@users.noreply.github.com>
2020-07-13 21:18:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+    const forms = document.querySelectorAll('form');
+
+    function postData(from) {
+        form.addEventListener('submit', () => {
+            
+        })
+    }
+})
